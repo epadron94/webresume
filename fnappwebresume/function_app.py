ffbcc907c62ff24f6ac64985867a2b987253f690
Erik Padrón <epadron94@icloud.com>
2025-04-18 15:35:55 -0600

    Initial commit AZF

---
+++ fnappwebresume/function_app.py
@@ -0,0 +1,46 @@
+import azure.functions as func
+import datetime
+import json
+import logging
+
+#PYTHON DEV MODEL FOR AZFUNCTIONS
+#la version 1  basado en functiones por carpetas
+# version2  permite manejar las AZF desde un solo FUNCTION APP, evitando codigo y config files repetidos
+
+
+# v2 model #
+
+
+app = func.FunctionApp() # registro central de las functions
+
+@app.route(route="roleDescriptions",methods=["GET"], auth_level=func.AuthLevel.FUNCTION)# cada function utiliza ste metodo para registrar su url, HTTP methods
+#Cuales son las config del app.route?
+def getRoleDescriptions(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            status_code=200
+        )
+
+@app.route(route="roleDescriptions", methods=["POST"], auth_level= func.AuthLevel.FUNCTION)
+def postRoleDescription(req: func.HttpRequest) -> func.HttpResponse:
+    try:
+        req_body = req.get_json()
+
+        name = req_body.get('name')
+        return func.HttpResponse(f"Hello,{name}")
+    except ValueError:
+        raise func.HttpResponse("Invalid input", status_code=400)
