74ffcc3e2b95af20404e4a1bbc7acb5bb3fa0fb7
Erik Padrón <epadron94@icloud.com>
2025-04-21 23:00:58 -0600

    testing workflow try 9

--- fnappwebresume/function_app.py
+++ fnappwebresume/function_app.py
@@ -28,7 +28,7 @@ def getRoleDescriptions(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. Hello.")
+        return func.HttpResponse(f"Hello, {name}. Hello. hello")
     else:
         return func.HttpResponse(
             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
