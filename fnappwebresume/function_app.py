31f34803bfeee2a6dc1e0157dbe3b4aab3470df9
Erik Padrón <epadron94@icloud.com>
2025-04-25 10:14:43 -0600

    rollback auth level anonymous

--- fnappwebresume/function_app.py
+++ fnappwebresume/function_app.py
@@ -28,7 +28,7 @@ def getRoleDescriptions(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. Hello. hello hello ")
+        return func.HttpResponse(f"Hello, {name}. https GET")
     else:
         return func.HttpResponse(
             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
@@ -41,7 +41,7 @@ def postRoleDescriptions(req: func.HttpRequest) -> func.HttpResponse:
         req_body = req.get_json()
 
         name = req_body.get('name')
-        return func.HttpResponse(f"Hello,{name}")
+        return func.HttpResponse(f"Hello,{name} POST")
     except ValueError:
         raise func.HttpResponse("Invalid input", status_code=400)
 
