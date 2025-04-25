a4f32de6dc01a66b77d4facdd32623096a07fd45
Erik Padrón <epadron94@icloud.com>
2025-04-25 11:57:09 -0600

    package deploy throubleshooting 3

--- fnappwebresume/function_app.py
+++ fnappwebresume/function_app.py
@@ -28,7 +28,7 @@ def getRoleDescriptions(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. https GET")
+        return func.HttpResponse(f"Hello, {name}. https GETT")
     else:
         return func.HttpResponse(
             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
